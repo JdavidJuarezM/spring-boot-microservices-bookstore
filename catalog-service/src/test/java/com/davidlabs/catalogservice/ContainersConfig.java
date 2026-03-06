eaccebedbb0b5b888633300f5e6db78842041ace
JdavidJuarezM <josedavidmagana@hotmail.com>
2026-03-06 14:34:41 -0600

    add database configuration and CI/CD setup for catalog service

--- catalog-service/src/test/java/com/davidlabs/catalogservice/ContainersConfig.java
+++ catalog-service/src/test/java/com/davidlabs/catalogservice/ContainersConfig.java
@@ -7,8 +7,7 @@ import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
+public class ContainersConfig {
     @Bean
     @ServiceConnection
     PostgreSQLContainer postgresContainer() {
